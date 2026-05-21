ce41361f0562f686b9b26250325bf752df722243
Marcel Wieśko <marcel.wiesko@icloud.com>
2026-05-21 15:07:54 +0200

    fix router basename

--- league/urls.py
+++ league/urls.py
@@ -3,25 +3,19 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register('teams', views.TeamViewSet)
-router.register('players', views.PlayerViewSet)
-router.register('matches', views.MatchViewSet)
-router.register('goals', views.GoalViewSet)
-
-router.register('leagues', views.LeagueViewSet)
-router.register('schedulers', views.SchedulerViewSet)
+router.register('teams', views.TeamViewSet, basename='teams')
+router.register('players', views.PlayerViewSet, basename='players')
+router.register('matches', views.MatchViewSet, basename='matches')
+router.register('goals', views.GoalViewSet, basename='goals')
+router.register('leagues', views.LeagueViewSet, basename='leagues')
+router.register('schedulers', views.SchedulerViewSet, basename='schedulers')
 
 urlpatterns = [
-
     path('api/', include(router.urls)),
-    path('api/table/', views.api_league_table, name='api_league_table'),
-    path('api/best-team/', views.api_best_team, name='api_best_team'),
-    path('api/best-player/', views.api_best_player, name='api_best_player'),
-    path(
-        'api/best-player-against-team/<int:team_id>/',
-        views.api_best_player_against_team,
-        name='api_best_player_against_team'
-    ),
-    path('api/add-goal/', views.api_add_goal),
     path('api/login/', views.api_login),
+    path('api/table/', views.api_league_table),
+    path('api/best-team/', views.api_best_team),
+    path('api/best-player/', views.api_best_player),
+    path('api/best-player-against-team/<int:team_id>/', views.api_best_player_against_team),
+    path('api/add-goal/', views.api_add_goal),
 ]
